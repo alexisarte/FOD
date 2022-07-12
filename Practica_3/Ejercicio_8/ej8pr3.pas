20a9ff3bee28d5eb0058f05fe2d543b448ff6ddc
alexisarte <alexisrodriguezarteaga@gmail.com>
2022-07-12 10:38:27 -0300

    Corrección

--- Practica_3/Ejercicio_8/ej8pr3.pas
+++ Practica_3/Ejercicio_8/ej8pr3.pas
@@ -12,96 +12,101 @@ type
 
     archivoDistro = File of distro;
 
-function existeDistribucion(var distros: archivoDistro; nombre: str50): boolean;
-    
-    function buscarDistribucion(var distros: archivoDistro; nombre: str50): Integer;
-    var
-        distro1: distro; pos: Integer;
-    begin
-        pos:= -1;
-        Seek(distros, 0);
-        while ((not eof(distros)) and (distro1.nombre <> nombre)) do begin
-            read(distros, distro1);
-        end;
-        if (distro1.nombre = nombre) then begin
-            pos:= FilePos(distros) - 1;
-        end;
-        buscarDistribucion:= pos;
-    end;
-
+procedure buscarDistribucion(var distros: archivoDistro; nombre: str50; var pos: Integer);
+var
+    distro1: distro;
 begin
-    if (buscarDistribucion(distros, nombre) = -1) then begin
-        existeDistribucion:= False;
+    reset(distros);
+    while ((not eof(distros)) and (distro1.nombre <> nombre)) do begin
+        read(distros, distro1);
     end;
+    if ((distro1.nombre = nombre) and (distro1.desarrolladores > 0)) then begin
+        pos:= (FilePos(distros)) - 1;
+    end;
+    close(distros);
 end;
 
+procedure existeDistribucion(var distros: archivoDistro; nombre: str50; var existe: boolean);
+var 
+    pos: integer;
+begin
+    pos:= 0;
+    buscarDistribucion(distros, nombre, pos);
+    if (pos <> 0) then begin
+        existe:= true;
+    end
+end;
 
 procedure altaDistribucion(var distros: archivoDistro);
 
     procedure leerDistribucion(var distro1: distro);
     begin
-        Write('Ingrese el nombre de la distro: ');
-        ReadLn(distro1.nombre);
-        Write('Ingrese el anio de lanzamiento: ');
-        ReadLn(distro1.lanzamiento);
-        Write('Ingrese el numero de version del kernel: ');
-        ReadLn(distro1.kernel);
-        Write('Ingrese la cantidad de desarrolladores: ');
-        ReadLn(distro1.desarrolladores);
-        Write('Ingrese la descripcion');
-        ReadLn(distro1.descripcion);
+        Write('Ingrese el nombre de la distro: '); ReadLn(distro1.nombre);
+        Write('Ingrese el anio de lanzamiento: '); ReadLn(distro1.lanzamiento);
+        Write('Ingrese el numero de version del kernel: '); ReadLn(distro1.kernel);
+        Write('Ingrese la cantidad de desarrolladores: '); ReadLn(distro1.desarrolladores); 
+        Write('Ingrese la descripcion'); ReadLn(distro1.descripcion);
     end;
 
 var
     distro1, distro2, distro3: distro;
     desarrolladores, pos: Integer;
+    existe: boolean;
 begin
     Reset(distros);
     Read(distros, distro1);
     leerDistribucion(distro3);
-    if (existeDistribucion(distros, distro3.nombre)) then begin
+    existeDistribucion(distros, distro3.nombre, existe);
+    if (existe) then begin
         WriteLn('Ya existe la distro');
     end
     else begin
         desarrolladores:= distro1.desarrolladores;
         if (desarrolladores < 0) then begin
-            pos:= Abs(desarrolladores);
+            pos:= Abs(desarrolladores); //
             Seek(distros, pos);
             Read(distros, distro2);
-            Seek(distros, pos);
+            Seek(distros, pos); //Se posiciona en la posicion de la distro anterior
             Write(distros, distro1);
             Seek(distros, 0);
             Write(distros, distro2);
-        end;
+        end
+        else if (desarrolladores = 0) then begin
+                Seek(distros, FileSize(distros));
+                Write(distros, distro3);
+            end;
     end;
 end;
 
 procedure bajaDistribucion(var distros: archivoDistro);
 var
-    distro1, distro2: distro; 
+    distro1, distroAEliminar: distro; 
     nombre: str50; pos: Integer;
+    existe: boolean;
 begin
     Write('Ingrese el nombre de la distro a eliminar: ');
     ReadLn(nombre);
+    Reset(distros);
     Read(distros, distro1);
-    if (existeDistribucion(distros, nombre)) then begin
-        pos:= FilePos(distros) - 1;
-        Seek(distros, pos);
-        Read(distros, distro2);
-        Seek(distros, pos);
-        Write(distros, distro1);
-        Seek(distros, 0);
-        distro2.desarrolladores:= pos * -1;
-        Write(distros, distro2);
+    existeDistribucion(distros, nombre, existe);
+    if (existe) then begin
+        buscarDistribucion(distros, nombre, pos);
+        Seek(distros, pos); //Se posiciona en la posicion de la distro a eliminar
+        Read(distros, distroAEliminar); //Se lee la distro a eliminar
+        Seek(distros, pos); //Se posiciona en la posicion de la distro a eliminar
+        Write(distros, distro1); //Copio el antiguo registro cabeza de la lista
+        Seek(distros, 0); //Se posiciona al principio del archivo
+        distroAEliminar.desarrolladores:= pos * -1; //Se le asigna un valor negativo al desarrolladores para que sea una distro eliminada
+        Write(distros, distroAEliminar); //Se escribe la distro eliminada en la cabecera del archivo
     end
     else begin
         WriteLn('Distro no existente');
     end;
+    Close(distros);
 end;
 
 var
     distros: archivoDistro;
 begin
-
     Assign(distros, 'distribuciones');
 end.
